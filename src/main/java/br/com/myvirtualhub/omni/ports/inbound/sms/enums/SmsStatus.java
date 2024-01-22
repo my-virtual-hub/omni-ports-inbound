56f6780a12cbad9eee3f99dc4da0bbc137f0dd6c
marco-quicula <marco.quicula@quicula.com.br>
2024-01-21 23:09:09 -0300

    Update SmsStatus enum and add comments to SmsPayloadDTO methods
    This commit extends the SmsStatus enum to include a new state OMNI_SENT, with comments explaining what this status denotes. Also, javadoc comments have been added to SmsPayloadDTO methods to describe what they do and to improve code readability. A redundant paragraph tag in InboundActionProviderFactory's initialisation method comment block has been removed.

--- src/main/java/br/com/myvirtualhub/omni/ports/inbound/sms/enums/SmsStatus.java
+++ src/main/java/br/com/myvirtualhub/omni/ports/inbound/sms/enums/SmsStatus.java
@@ -11,5 +11,12 @@ package br.com.myvirtualhub.omni.ports.inbound.sms.enums;
  * This enum represents the status of an SMS message.
  */
 public enum SmsStatus {
-    OMNI_QUEUED
+    /**
+     * Represents the status of an SMS message when it is queued in the Omni SMS system.
+     */
+    OMNI_QUEUED,
+    /**
+     * This enum value represents the status of an SMS message that has been successfully sent by the Omni SMS system.
+     */
+    OMNI_SENT
 }
