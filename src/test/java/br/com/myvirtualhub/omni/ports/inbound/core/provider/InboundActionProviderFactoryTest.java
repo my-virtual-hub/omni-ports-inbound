72e0b91a42697d49a06ca13f8f14e24ab19124f5
marco-quicula <marco.quicula@quicula.com.br>
2024-01-23 13:04:00 -0300

    Update project license to Apache 2.0
    The project license has been changed from BSD 2-Clause License to Apache 2.0 license. All the copyright notices and license statements in each file of the source code and the README file have been updated accordingly to reflect this change. Also, the LICENSE file content has been updated to Apache 2.0 license terms and conditions.

--- src/test/java/br/com/myvirtualhub/omni/ports/inbound/core/provider/InboundActionProviderFactoryTest.java
+++ src/test/java/br/com/myvirtualhub/omni/ports/inbound/core/provider/InboundActionProviderFactoryTest.java
@@ -1,14 +1,23 @@
 /*
- * Copyright (c) 2024.
+ * Copyright 2024 My Virtual Hub
  *
- *  This software is provided under the BSD-2-Clause license. By using this software,
- *  * you agree to respect the terms and conditions of the BSD-2-Clause license.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package br.com.myvirtualhub.omni.ports.inbound.core.provider;
 
 import br.com.myvirtualhub.omni.ports.inbound.core.exceptions.ProviderFactoryException;
-import br.com.myvirtualhub.omni.ports.inbound.core.interfaces.InboudActionFactory;
+import br.com.myvirtualhub.omni.ports.inbound.core.interfaces.InboundActionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -29,9 +38,9 @@ class InboundActionProviderFactoryTest {
 
     @Test
     void testInitialize() throws ProviderFactoryException {
-        InboudActionFactoryTestImpl inboudActionFactoryTest = new InboudActionFactoryTestImpl();
+        InboundActionFactoryTestImpl inboudActionFactoryTest = new InboundActionFactoryTestImpl();
         inboundActionProviderFactoryUnderTest.initialize(List.of(inboudActionFactoryTest));
-        InboudActionFactory<?> result = inboundActionProviderFactoryUnderTest.getFactory("InboudActionFactory");
+        InboundActionFactory<?> result = inboundActionProviderFactoryUnderTest.getFactory("InboundActionFactory");
         assertNotNull(result);
         assertEquals(inboudActionFactoryTest.getClass(), result.getClass());
     }
@@ -46,10 +55,10 @@ class InboundActionProviderFactoryTest {
     void testGetFactory_ThrowsProviderFactoryException() {
         inboundActionProviderFactoryUnderTest.resetProviderFactory();
         assertThrows(ProviderFactoryException.class,
-                () -> inboundActionProviderFactoryUnderTest.getFactory("InboudActionFactory"));
+                () -> inboundActionProviderFactoryUnderTest.getFactory("InboundActionFactory"));
     }
 
-    static class InboudActionFactoryTestImpl implements InboudActionFactory<Object> {
+    static class InboundActionFactoryTestImpl implements InboundActionFactory<Object> {
         @Override
         public Object create() {
             return null;
